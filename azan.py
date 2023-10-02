42e4f6eec08d9587d3647019cdc86a12d27e4a00
Abolfazl mehrabi <mehrabiabol1383@gmail.com>
2023-10-02 21:48:43 +0330

    script cleaded and commented

--- azan.py
+++ azan.py
@@ -1,42 +1,52 @@
+##  ## ## ## ## ## ## ## ## ##  ##
+#                                #
+#   this is a simple logic for   #
+#    calling to prayer 'azan'    #
+#                                #
+##  ## ## ## ## ## ## ## ## ##  ##
+
 from time import sleep
 from datetime import datetime
-import os
+from pydub import AudioSegment
+from pydub.playback import play
 import requests
 import re
 from bs4 import BeautifulSoup
-def totime(n):
-    min = int(n[3:])
-    hour = int(n[:2])
-    min += 30
-    if min >= 60:
-        min -= 60
-        hour += 1
-    hour += 3
-    if hour < 10:
-        hour = '0' + str(hour)
-    if min < 10:
-        min = '0' + str(min)
-    return str(hour) + ':' + str(min)
-res = requests.get('https://badesaba.ir/owghat/1184/%D9%87%D9%85%D8%AF%D8%A7%D9%86')
-if res.status_code == 200:
-    print("operation done!")
-    soup = BeautifulSoup(res.text, 'html.parser')
-    all_times = soup.find_all('p', attrs={'class': 'time'})
-    li_times = []
-    for time in all_times:
-        li_times.append(str(re.sub(r'<p _ngcontent-sc88="" class="time"> 'and r' </p>','', time.text)).strip())
+
+def table(times:list) -> None : # to printing an 'azan' table
     table = '-' * 21
     azan = ['Sobhh','tolu`', 'zohhrr', 'aasrr','qorub' , 'maqreb', 'ei`sa', 'nimeh']
     print(f'+ {table} +')
     for i in range(len(azan)):
-        print(f'|  {azan[i]}\t{li_times[i]}   |')
+        print(f'|  {azan[i]}\t{times[i]}   |')
     print(f'+ {table} +')
-    
+
+
+# scrapping from 'badesaba' (an iranian islamic calendar with intersting features =))
+
+res = requests.get('https://badesaba.ir/owghat/1184/%D9%87%D9%85%D8%AF%D8%A7%D9%86')
+
+
+# if scrapping done successfully
+
+if res.status_code == 200:
+    soup = BeautifulSoup(res.text, 'html.parser')
+    ini_times = soup.find_all('p', attrs={'class': 'time'}) # getting all azan times
+    main_times = []  # for storing trimmed azan times
+    for time in ini_times:
+        # trimming initial azan times
+        main_times.append(str(re.sub(r'<p _ngcontent-sc88="" class="time"> 'and r' </p>','', time.text)).strip())
+
+    table(main_times)
+
     while True:
+        # checking the current time to identify azan time
         time = (str(datetime.now()).split('-'))[2][3:8]
-        for t in li_times:
+        for t in main_times:
             if time == t:
-                azan = "Azan.mp3"
-                os.system("mpg123 " + azan)
+                song = AudioSegment.from_mp3("Azan.mp3")
+                play(song)        
 else:
-    print("cannot scrape from 'badesaba'\n\ntry to do it from another way . . . ")
+    print("cannot scrape from 'badesaba'")
+
+# thank you for your attention and spending time #
